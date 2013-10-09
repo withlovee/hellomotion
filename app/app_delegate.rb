1a7d61cb3c27b7491b06c2b1f24fa0d2e5e9c455
Vee <happierv@gmail.com>
2013-10-09 15:12:22 +0700

    Chapter 1 - HelloMotion

---
+++ app/app_delegate.rb
@@ -0,0 +1,10 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+  	alert = UIAlertView.new
+  	alert.message = "Hello Motion!"
+  	alert.show
+
+  	puts "Hello again!"
+    true
+  end
+end
